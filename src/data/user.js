f07105db7c10ca6987eb66ae31bdfd87a5f30928
chandan <chandanchandan51218@gmail.com>
2024-05-17 19:39:20 +0530

    yae boy

---
+++ src/data/user.js
@@ -0,0 +1,64 @@
+const INFO = {
+	main: {
+		title: "Chandan portfolio",
+		name: "chandan kumar km",
+		email: "Chandankumar8150008672@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://x.com/Chandan67999581",
+		github: "https://github.com/chandan-kumar-km/",
+		linkedin: "https://www.linkedin.com/in/chandan-kumar-km/",
+		instagram: "https://www.instagram.com/karlekar.moto/",
+		stackoverflow: "https://stackoverflow.com/users/25096395/chandan",
+		facebook: "https://www.facebook.com/profile.php?id=100020879677966",
+	},
+
+	homepage: {
+		title: "Versatile Software Engineer | Front-end Developer | Electrical and Electronics Engineering Graduate",
+		description:
+			"Skilled frontend developer with expertise in modern web technologies including React, Redux, JavaScript (ES6+), HTML5, and CSS3. Proficient in building responsive and visually appealing user interfaces with a focus on optimal user experience. Experienced in developing single-page applications, integrating RESTful APIs, and implementing state management solutions. Adept in leveraging tools and frameworks such as Webpack, Babel, and Bootstrap to streamline development workflows. Strong problem-solving skills and a collaborative team player with a keen eye for detail. Passionate about creating innovative and engaging web experiences that delight users.",
+	},
+
+	about: {
+		title: "I’m chandan. I live in india, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "BLOG-WEBSITE",
+			description:
+				"Developed a dynamic blogging platform using React, Redux, and Appwrite, enabling users to create, edit, and publish content seamlessly.Implemented image integration within blog posts, enhancing visual storytelling capabilities and user engagement.Demonstrated proficien",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://blog-eight-dun-42.vercel.app/",
+		},
+		{
+			title: "NEWS-WEBSITE",
+			description:
+				"Developed a real-time news application sourcing updates from NewsAPI.org, featuring an infinite scroll for seamless loading of over 100 articles. Integrated NewsAPI.org to fetch news across 10 diverse categories, ensuring comprehensive coverage. Utilized JavaScript techniques for efficient API handling and data retrieval of 500+ articles.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/chandan-kumar-km/News-app-React",
+		},
+		{
+			title: "SMART SECURITY POLE",
+			description:
+				"Led the implementation of a drone detection system using advanced shape and color recognition technologies, capable of detecting drones within a 25-meter radius. Applied cutting-edge algorithms to identify and differentiate between 8 different drone models, showcasing proficiency in computer vision and image processing. Successfully integrated detection mechanisms with 3 alarm systems and 2 messaging protocols for timely threat alerts.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/chandan-kumar-km/smart_security_pole",
+		},
+	],
+};
+
+export default INFO;
