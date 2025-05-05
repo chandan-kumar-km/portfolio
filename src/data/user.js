ec34d09ce274b636fcc4b1f91fd9648f7aceeed8
Chandan Kumar <chandanchandan51218@gmail.com>
2025-05-05 22:05:10 +0530

    Initial commit after manual download

---
+++ src/data/user.js
@@ -0,0 +1,73 @@
+const INFO = {
+	main: {
+		title: "Chandan portfolio",
+		name: "chandan kumar km",
+		email: "Chandankumar8150008672@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://x.com/Chandan67999581",
+		github: "https://github.com/chandan-kumar-km/",
+		linkedin: "https://www.linkedin.com/in/chandan-kumar-km/",
+		instagram: "https://www.instagram.com/karlekar.moto/",
+		stackoverflow: "https://stackoverflow.com/users/25096395/chandan",
+		facebook: "https://www.facebook.com/profile.php?id=100020879677966",
+	},
+
+	homepage: {
+		title: "Engineer | Developer | Electrical and Electronics Engineering Graduate",
+		description:
+			"Skilled frontend developer with expertise in modern web technologies including Nextjs React, Redux , Zustand , JavaScript (ES6+), HTML5, and CSS3. Proficient in building responsive and visually appealing user interfaces with a focus on optimal user experience. Experienced in developing single-page applications, integrating RESTful APIs, and implementing state management solutions. Adept in leveraging tools and frameworks such as Webpack, Babel, and Bootstrap to streamline development workflows. Strong problem-solving skills and a collaborative team player with a keen eye for detail. Passionate about creating innovative and engaging web experiences that delight users.",
+	},
+
+	about: {
+		title: "I’m chandan. I live in india, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "code-Clarify",
+			description:
+				"CodeClarify is a Stack Overflow-inspired Q&A platform built using TypeScript, Next.js, and Zustand. It serves as a collaborative space for developers to seek and share knowledge, fostering a community-driven environment for problem-solving and learning. The application is designed to facilitate user interactions, allowing individuals to post questions, submit answers, and engage with content through upvotes and tags. The responsive UI and intuitive UX replicate real-world community interactions, enhancing user experience. Emphasis was placed on type safety, state management, and performance optimization, showcasing strong front-end architecture and scalable design practices.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/typescript/typescript.png",
+			linkText: "view Project",
+			link: "https://stackover-flow-clone-weld.vercel.app/",
+		},
+		{
+			title: "BLOG-WEBSITE",
+			description:
+				"Developed a dynamic blogging platform using React, Redux, and Appwrite, enabling users to create, edit, and publish content seamlessly.Implemented image integration within blog posts, enhancing visual storytelling capabilities and user engagement.Demonstrated proficien",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://blog-eight-dun-42.vercel.app/",
+		},
+		{
+			title: "NEWS-WEBSITE",
+			description:
+				"Developed a real-time news application sourcing updates from NewsAPI.org, featuring an infinite scroll for seamless loading of over 100 articles. Integrated NewsAPI.org to fetch news across 10 diverse categories, ensuring comprehensive coverage. Utilized JavaScript techniques for efficient API handling and data retrieval of 500+ articles.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/chandan-kumar-km/News-app-React",
+		},
+		{
+			title: "password generator",
+			description:
+				"Led the development of a secure password generator app using JavaScript and React. Implemented a robust algorithm to generate strong passwords adhering to security guidelines. Integrated React components for an intuitive UI, allowing customization and one-click password copying. Leveraged React's state management and JavaScript's cryptographic functions for efficiency and enhanced security. Implemented client-side validation and collaborated with the design team for an accessible user experience.",
+
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://password-generator-nu-woad.vercel.app/",
+		},
+	],
+};
+
+export default INFO;
